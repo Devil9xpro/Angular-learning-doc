8b1c1011586d2a61fa0f55a5bf2c7dc590475af1
David Nguyen <chinhzz3107@gmail.com>
2021-11-05 14:27:44 +0700

    add service, service in service, observable to get async data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,5 @@
+import { MessageService } from './../message.service';
+import { HeroService } from './../hero.service';
 import { HEROES } from './../mock-heroes';
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
@@ -8,16 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES
+  heroes: Hero[] = []
   selectedHero?: Hero
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+    this.messageService.add(`HeroComponent: Selected hero id=${hero.id}`)
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 
 }
