7c9f5286b6ae6f1a336792431a66989c36030458
David Nguyen <chinhzz3107@gmail.com>
2021-11-08 08:56:12 +0700

    add router, router-link to navigate

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
+import { HeroService } from './../hero.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Hero } from './../hero';
 import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,11 +10,26 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero
+  hero: Hero | undefined
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero()
   }
 
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"))
+    var a = this.heroService.getHero(id)
+    console.log(a)
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero)
+  }
+
+  goBack() {
+    this.location.back()
+  }
 }
